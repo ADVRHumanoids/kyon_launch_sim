06e447466e273b3e73c3633d331a0198c2b1454c
JVALPASS <val1212.pass@gmail.com>
2025-04-01 11:56:50 +0200

    update

--- src/docker/setup.sh
+++ src/docker/setup.sh
@@ -3,6 +3,6 @@ ros1(){
     cd $SCRIPT_DIR
     xhost +local:root
     echo "Running docker image from $PWD..."
-    docker compose up ${1:-dev} -d --no-recreate
-    docker compose exec ${1:-dev} bash
+    docker compose -p kyon-cetc-focal-ros1 up ${1:-dev} -d --no-recreate
+    docker compose -p kyon-cetc-focal-ros1 exec ${1:-dev} bash
 }
